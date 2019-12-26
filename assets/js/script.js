0f5261a37143c96ba704ba01b02219393cd1d2f1
shelby palumbo <shelbypalumbo94@gmail.com>
2019-12-26 18:57:26 -0500

    Created the date/time stamp for the Day Planner heading.

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+//Prints the current date to the id currentDay ---------------------------------------    
+var date = moment().format("dddd, MMMM Do YYYY");
+$("#currentDay").html(date); 
+//------------------------------------------------------------------------------------
+    
+    
+
+
+
+
+
+
+
+});
