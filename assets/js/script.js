a13e8c2c66da7761dc418454f68161fabe3ba60b
shelby palumbo <shelbypalumbo94@gmail.com>
2019-12-27 20:26:40 -0500

    Added columns to time-block divs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,26 @@ $(document).ready(function () {
 
 //Prints the current date to the id currentDay ---------------------------------------    
 var date = moment().format("dddd, MMMM Do YYYY");
-$("#currentDay").html(date); 
+$("#currentDay").text(date); 
 //------------------------------------------------------------------------------------
+var currentTime = moment().format("h:mm:ss a");
+var startTime = moment().format('09:00'); //9am
+var endTime = moment().format('18:00');  //6pm
+
+
+$(function(){
+    if(currentTime > startTime){
+    $(".time-block").addClass("past");
+    }
+});
+
+
+$(function(){
+    if(currentTime < endTime){
+    $(".time-block").addClass("future");
+    }
+});
 
 
 
-  
 })
