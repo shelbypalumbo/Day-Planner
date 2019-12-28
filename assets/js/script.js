146f3112c98db7c83daf25b84819eaba41b99351
shelby palumbo <shelbypalumbo94@gmail.com>
2019-12-28 15:11:15 -0500

    finished if statements for changing time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 var date = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(date); 
 //------------------------------------------------------------------------------------
-    
+  
 var currentTime = moment().format("h");
 
 var hour9am = $("#hour9am").text();
@@ -19,6 +19,12 @@ var hour1pm = $("#hour1pm").text();
   hour1pm = hour1pm[0];
 var hour2pm = $("#hour2pm").text();
   hour2pm = hour2pm[0];
+var hour3pm = $("#hour3pm").text();
+  hour3pm = hour3pm[0];
+var hour4pm = $("#hour4pm").text();
+  hour4pm = hour4pm[0];
+var hour5pm = $("#hour5pm").text();
+  hour5pm = hour5pm[0];
 
 
 
@@ -76,23 +82,24 @@ if (hour3pm < currentTime){
   $("#hour15 .description ").addClass("present");  
 } else if (hour3pm > currentTime){
   $("#hour15 .description ").addClass("future");  
-}
+};
 
 if (hour4pm < currentTime){
   $("#hour16 .description ").addClass("past");  
-} else if( hour3pm == currentTime){
+} else if( hour4pm == currentTime){
   $("#hour16 .description ").addClass("present");  
-} else if (hour3pm > currentTime){
+} else if (hour4pm > currentTime){
   $("#hour16 .description ").addClass("future");  
 }
 
 if (hour5pm < currentTime){
   $("#hour17 .description ").addClass("past");  
-} else if( hour3pm == currentTime){
+} else if( hour5pm == currentTime){
   $("#hour17 .description ").addClass("present");  
-} else if (hour3pm > currentTime){
+} else if (hour5pm > currentTime){
   $("#hour17 .description ").addClass("future");  
 }
+
 
 
 })
