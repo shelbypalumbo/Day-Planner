55b8d504a3cb1484894002e6a30716011d896547
shelby palumbo <shelbypalumbo94@gmail.com>
2020-01-03 13:45:28 -0500

    Class styling functionality finished. Working on input storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,40 +5,45 @@ var date = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(date); 
 
 //Current, start and end time variables------------------------------------------------
-var currentTime = moment().format("hh a");
-var endTime = moment("18", 'HH').format('hh a');
-var startTime = moment("09", 'HH').format('h a');
+var currentTime = moment().format("HH");
 //-------------------------------------------------------------------------------------
 
-var dayHrs = 18;
+var dayHrs = 19;
 
 for( var i = 9; i < dayHrs; i++){
     var timeBlock = $("<div class='time-block row'>");
     var hour = $("<div class='hour col-1'>");
     var task = $("<textarea class='description col-10'>");
+    task.attr("data-task", i);
     var save = $("<button class='saveBtn col-1'>").append("<i class='fas fa-save'>");
-    
     $(".container").append(timeBlock);
     timeBlock.append(hour, task, save);
     hour.append(i + ":00");
-}
 
-$(function() {
-    if(hour < currentTime){
-    $(".time-block").addClass("past");
-    }
-    else if(currentTime = hour){
-    $(".time-block").addClass("present");
-    }
-    else if(hour > currentTime){
-        $(".time-block").addClass("future");
-        }
-});
+    if(currentTime > i){
+      timeBlock.addClass("past");
+      }
+      else if(currentTime == i){
+      timeBlock.addClass("present");
+      }
+      else if(currentTime < i){
+      timeBlock.addClass("future");
+      }
+      //console.log(i);
+      //console.log(currentTime);
+};
 
+//Storage------------------------------------------------------------------------------
+$(function(){
+    var task = localStorage.getItem("task");
+    var textArea = $(".description");
+    textArea.text(task);
+    console.log(task);
+  });
 
-console.log(currentTime);
-console.log(startTime);
-console.log(endTime);
-console.log(e);
-
+$("button").on("click", function(event) {
+    event.preventDefault();
+    var task = $("data-task", i);
+      localStorage.setItem("task", task);
+    });
 })
